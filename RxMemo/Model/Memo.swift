579f36a5c58f07d73fbc402b1aad9536d51b8c12
nayoung <ny2322@gmail.com>
2021-06-29 11:42:32 +0900

    add memo model

---
+++ RxMemo/Model/Memo.swift
@@ -0,0 +1,25 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by Nayomi on 6/29/21.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
