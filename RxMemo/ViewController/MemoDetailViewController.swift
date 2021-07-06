39de53e7df2f743ec3f026d62c736774063cf7d4
nayoung <ny2322@gmail.com>
2021-07-06 20:12:00 +0900

    list -> detail fix transition issue

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -48,6 +48,8 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
                 }
             }
             .disposed(by: rx.disposeBag)
+        
+        
     }
 
 }
